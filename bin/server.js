662ea6a2c9ee13a25d2d7e3f4a09470770f9a491
stankowalczyk <stankowalczyk@users.noreply.github.com>
2016-08-03 18:52:35 +1000

    NEW: Initial commit

---
+++ bin/server.js
@@ -0,0 +1,29 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import moment from 'moment';
+
+let app = express();
+
+// Ensure our app uses body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const PORT = 8080;
+
+let router = express.Router();
+
+router.use((req, res, next) => {
+  console.log(`${moment().toString()}\t${req.method}\t${req.baseUrl}`);
+  next();
+});
+
+router.get('/', (req, res) => {
+  res.status(200).send('RESTful-LIFX - A NodeJS RESTful API for interacting with your LIFX bulbs locally');
+});
+
+app.use('/api', router);
+
+app.listen(PORT);
+console.log('RESTful-LIFX server started');
+console.log(`Server listening on port: ${PORT}`);
+console.log(`Log Time\t\t\t\tMethod\tURL`);
